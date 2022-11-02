383634f44c49a2e86c5cda6152a5f15bec591724
matheo-w <116657976+matheo-w@users.noreply.github.com>
2022-11-02 16:29:08 +0100

    nach Preis suchen + while-loop
    Suche nach einem genauen Preis wurde hinzugefügt und eine while-loop, mit der immer wieder nach der location gefragt wird

--- airbnb_bot/airbnb_bot.py
+++ airbnb_bot/airbnb_bot.py
@@ -35,6 +35,26 @@ def get_location_from_input(sentence, regex_list=location_regex):
     # return None if no regular expression matches the input
     return None
 
+# zwei neue Funktionen. Die weiter_fragen() hatten wir letzte Woche zusammen gemacht die andere wandelt den Input nur in einen Integer um
+def get_max_money_from_input(sentence):
+    number = int(sentence)
+    return number
+
+def weiter_fragen(results,top_n):
+    sentence2 = input('\nMöchtest du 10 weitere Ergebnisse sehen?\n')
+    sentence2 = sentence2.lower()
+
+    if sentence2 == 'ja': 
+        print('Alles klar, hier sind 10 weitere Ergebnisse:\n'.format(top_n))
+        for r in results[10:20]:
+            answer = '"{}", {}. Das Apartment kostet {}€.'.format(r[0], r[1], r[2])
+            # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
+            print(answer)
+    else:
+        print('Dann halt nicht')
+
+        return
+
 
 def query_sql(key, value, columns, sql_file):
     """
@@ -48,11 +68,12 @@ def query_sql(key, value, columns, sql_file):
 
 
     # prepare query string
-    query_template = 'SELECT {columns} FROM listings WHERE {key} = "{value}"'
+    query_template = 'SELECT {columns} FROM listings WHERE {key} = "{value}"' #AND ...  
     columns_string = ', '.join(columns)  # e.g. [location, price] -> 'location, price'
     # replace the curly brackets in query_template with the corresponding info
     query = query_template.format(columns=columns_string, key=key, value=value)
 
+    
     # execute query
     r = c.execute(query)
     # get results as list
@@ -62,6 +83,7 @@ def query_sql(key, value, columns, sql_file):
     conn.close()
 
     return results
+
 
 
 def airbnb_bot(sql_file, top_n): 
@@ -95,12 +117,17 @@ def airbnb_bot(sql_file, top_n):
         'Reinickendorf', 'Spandau', 'Steglitz - Zehlendorf',
         'Tempelhof - Schöneberg', 'Treptow - Köpenick']
     print('Wir haben Appartements in folgenden Stadtteilen:')
-    print(', '.join(neighbourhoods))
+    print(', '.join(neighbourhoods)) # liste wird zu einem String gemacht
 
-    # get query from user
-    sentence = input('\nWo möchtest du denn übernachten?\n')
-    # normalize to lowercase
-    sentence = sentence.lower()
+    # get query from user / usereingabe wird als var gespeichert
+
+# Neuer Teil - Matheo 
+# 2 Auswahlmöglichkeiten/Kriterien um was geeignetes zu finden
+    q1 = input('\nMöchtest du nach Stadtteil oder Preis suchen?\n')
+    # normalize to lowercase / wird kleingeschrieben übernommen, da location_regex alles kleingeschrieben braucht (es müssen weniger Fälle bedacht werden)
+    q1 = q1.lower()
+
+
 
     #####################################################################
     # STEP 2: extract location information and check whether it's valid #
@@ -108,32 +135,194 @@ def airbnb_bot(sql_file, top_n):
 
 
 
-    # NLU -SPRACHVERSTEHEN
+    # NLU -SPRACHVERSTEHEN 
 
-    # extract location from user input
-    location = get_location_from_input(sentence)
+# Suche nach Stadtteil ist gleichgeblieben
+    if q1 == 'stadtteil':
+        # extract location from user input
+        sentence = input('\nWo möchtest du übernachten?\n')
+        sentence = sentence.lower()
+        location = get_location_from_input(sentence)
 
-    if location is None:
-        # if the user input doesn't contain valid location information:
-        # apologize & quit
-        print('\nEntschuldigung, das habe ich leider nicht verstanden...')
-        return
+        while location is None: # aus der if-loop ist eine while-loop gemacht worden, damit die frage immer wieder gestellt wird,bis eine zufriedenstellende antwort gegeben wurde
+            # if the user input doesn't contain valid location information:
+            # apologize & quit
+            print('\nEntschuldigung, das habe ich leider nicht verstanden...')
+            sentence = input('\nWo möchtest du denn übernachten?\n')
+            sentence = sentence.lower()
+            location = get_location_from_input(sentence)
+
+            # get matches from csv file
+        columns = ['name', 'neighbourhood', 'price'] # versteh ich nicht
+        results = query_sql(
+                key='neighbourhood_group', value=location, # es wird in neighbourhood_group nach der location gesucht
+                columns=columns, sql_file=sql_file
+        )
+
+        # if there are no results: apologize & quit
+        if len(results) == 0:
+            print('Tut mir Leid, ich konnte leider nichts finden!')
+            return
+
+        print('Ich habe {} passende Wohnungen in {} gefunden.\n'.format(
+        len(results), location))
+        print('Hier sind die {} besten Ergebnisse:\n'.format(top_n))
+
+
+    # print the first top_n entries from the results list
+        for r in results[:top_n]: # für jeden der top 10 Einträge wird der Satz geprinted und mit name, neighbourhood und price gefüllt
+            answer = '"{}", {}. Das Apartment kostet {}€.'.format(
+                # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
+                r[0], r[1], r[2] 
+            )
+            print(answer)
+        weiter_fragen(results,top_n) # hier wird gefragt, ob 10 weitere ergebnisse gezeigt werden sollen
+
+
+# hier wird nach einem Budget gefragt, es werden nur Unterkünfte gezeigt, die für den angegebenen Betrag eingestellt wurden
+    elif q1 == 'preis':
+        q2 = input('\nWie groß ist dein Budget?\n')
+        max_money = get_max_money_from_input(q2)
+
+        columns = ['name', 'neighbourhood', 'price','neighbourhood_group']
+        results = query_sql(
+                key='price', value=max_money, 
+                columns=columns, sql_file=sql_file)
+        print('Ich habe {} passende Wohnungen für genau {}€ gefunden.\n'.format(
+        len(results), max_money))
+        print('Hier sind die besten 10 Ergebnisse:')
+
+        while len(results) == 0:
+            print('Tut mir Leid, ich konnte leider nichts finden!')
+            qw = input('\nMöchtest du nach einer anderen Preisklasse suchen?\n')
+            if qw == 'ja':
+                q2 = input('\nWie groß ist dein Budget?\n')
+                max_money = get_max_money_from_input(q2)
+                columns = ['name', 'neighbourhood', 'price','neighbourhood_group'] 
+                results = query_sql(
+                key='price', value=max_money,
+                columns=columns, sql_file=sql_file)
+                print('Ich habe {} passende Wohnungen für genau {}€ gefunden.\n'.format(
+                len(results), max_money))
+                print('Hier sind die 10 besten Ergebnisse:')
+
+            if qw == 'nein':
+                print('Dann kann ich dir auch nicht mehr helfen.')
+                return
+
+
+        for r in results[:top_n]: # für jeden der top 10 Einträge wird der Satz geprinted und mit name, neighbourhood und price gefüllt
+            answer = '"{}", Im Stadtteil {}.'.format(
+                # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
+                r[0], r[3]
+            )
+            print(answer)
+        weiter_fragen(results,top_n)
+
+
+# falls die erste Eingabe fehlerhaft war, wird nochmal gefragt. Eigentlich sollte dann immer wieder gefragt werden, aber ich weiß nicht wie
+
+    if q1 != 'stadtteil' and q1 != 'preis': # while-loop, damit immer wieder nachgefragt werden kann?
+        print('Sorry, das steht nicht zur Auswahl. Du musst dich schon für eins entscheiden.')
+        q1 = input('\nAlso. Stadtteil oder Preis?\n')
+        q1 = q1.lower()
+
+        
+
+        if q1 == 'stadtteil':
+            # extract location from user input
+            sentence = input('\nWo möchtest du übernachten?\n')
+            sentence = sentence.lower()
+            location = get_location_from_input(sentence)
+
+            while location is None: # aus der if-loop ist eine while-loop gemacht worden, damit die frage immer wieder gestellt wird,bis eine zufriedenstellende antwort gegeben wurde
+                # if the user input doesn't contain valid location information:
+                # apologize & quit
+                print('\nEntschuldigung, das habe ich leider nicht verstanden...')
+                sentence = input('\nWo möchtest du denn übernachten?\n')
+                sentence = sentence.lower()
+                location = get_location_from_input(sentence)
+
+                # get matches from csv file
+            columns = ['name', 'neighbourhood', 'price'] # versteh ich nicht
+            results = query_sql(
+                    key='neighbourhood_group', value=location, # es wird in neighbourhood_group nach der location gesucht
+                    columns=columns, sql_file=sql_file
+            )
+
+            # if there are no results: apologize & quit
+            if len(results) == 0:
+                print('Tut mir Leid, ich konnte leider nichts finden!')
+                return
+
+            print('Ich habe {} passende Wohnungen in {} gefunden.\n'.format(
+            len(results), location))
+            print('Hier sind die {} besten Ergebnisse:\n'.format(top_n))
+
+
+        # print the first top_n entries from the results list
+            for r in results[:top_n]: # für jeden der top 10 Einträge wird der Satz geprinted und mit name, neighbourhood und price gefüllt
+                answer = '"{}", {}. Das Apartment kostet {}€.'.format(
+                    # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
+                    r[0], r[1], r[2] 
+                )
+                print(answer)
+            weiter_fragen(results,top_n)
+
+
+
+        elif q1 == 'preis':
+            q2 = input('\nWie groß ist dein Budget?\n')
+            max_money = get_max_money_from_input(q2)
+
+            columns = ['name', 'neighbourhood', 'price', 'neighbourhood_group']
+            results = query_sql(
+                    key='price', value=max_money, 
+                    columns=columns, sql_file=sql_file)
+            print('Ich habe {} passende Wohnungen für genau {}€ gefunden.\n'.format(
+            len(results), max_money))
+
+            while len(results) == 0:
+                print('Tut mir Leid, ich konnte leider nichts finden!')
+                qw = input('\nMöchtest du nach einer anderen Preisklasse suchen?\n')
+                if qw == 'ja':
+                    q2 = input('\nWie groß ist dein Budget?\n')
+                    max_money = get_max_money_from_input(q2)
+                    columns = ['name', 'neighbourhood', 'price', 'neighbourhood_group'] 
+                    results = query_sql(
+                    key='price', value=max_money,
+                    columns=columns, sql_file=sql_file)
+                    print('Ich habe {} passende Wohnungen für genau {}€ gefunden.\n'.format(
+                    len(results), max_money))
+
+                if qw == 'nein':
+                    print('Dann kann ich dir auch nicht mehr helfen.')
+                    return
+
+            for r in results[:top_n]: # für jeden der top 10 Einträge wird der Satz geprinted und mit name, neighbourhood und price gefüllt
+                answer = '"{}", {}.'.format(
+                    # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
+                    r[0], r[3]
+                )
+                print(answer)
+            weiter_fragen(results,top_n)
+
+
+    # else:
+    #     print('Sorry, das steht nicht zur Auswahl. Du musst dich schon für eins entscheiden.')
+
+
+
+
+
+
+
 
     #####################################################################
     # STEP 3: query sqlite file for flats in the area given by the user #
     #####################################################################
 
-    # get matches from csv file
-    columns = ['name', 'neighbourhood', 'price']
-    results = query_sql(
-            key='neighbourhood_group', value=location,
-            columns=columns, sql_file=sql_file
-        )
 
-    # if there are no results: apologize & quit
-    if len(results) == 0:
-        print('Tut mir Leid, ich konnte leider nichts finden!')
-        return
 
 
     #############################################################################
@@ -143,33 +332,11 @@ def airbnb_bot(sql_file, top_n):
     # NLG- Sprachgenerierung
 
     # return results
-    print('Ich habe {} passende Wohnungen in {} gefunden.\n'.format(
-        len(results), location))
-    print('Hier sind die {} besten Ergebnisse:\n'.format(top_n))
 
-    # print the first top_n entries from the results list
-    for r in results[:top_n]:
-        answer = '"{}", {}. Das Apartment kostet {}€.'.format(
-            # look at the columns list to see what r[0], r[1], r[2] are referring to!
-            r[0], r[1], r[2]
-        )
-        print(answer)
-
-
-    sentence2 = input('\nMöchtest du 10 weitere Ergebnisse sehen?\n')
-    sentence2 = sentence2.lower()
-
-    if sentence2 == 'ja': # mehr antwortmöglichkeiten anbieten?
-        print('Hier sind die {} besten Ergebnisse:\n'.format(top_n))
-        for r in results[10:20]:
-            answer = '"{}", {}. Das Apartment kostet {}€.'.format(r[0], r[1], r[2])
-            # look at the columns list to see what r[0], r[1], r[2] are referring to! r[0] = name / r[1] = neighborhood / r[2] = price
-            print(answer)
-    else:
-        print('Dann halt nicht.')
-        return
+        
 
 
 if __name__ == '__main__':
     #  the airbnb_bot() function is called if the script is executed!
-    airbnb_bot(sql_file='listings.db', top_n=10)
+    airbnb_bot(sql_file='listings.db', top_n=10) # argumente: angeben, welche sql datei verwendet werden soll, wie viele ergebnisse angezeigt werden sollen
+
